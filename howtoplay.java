2371284f1791d2ddb88f3a0a2610aa2435a0459c
BanDev <141050405+mr123ivan@users.noreply.github.com>
2023-12-06 17:06:31 +0800

    Create howtoplay.java

---
+++ howtoplay.java
@@ -0,0 +1,38 @@
+package com.mycompany.dota3;
+
+/**
+ *
+ * @author mashp
+ */
+public class howtoplay {
+    
+ public void display(){
+    
+       System.out.println("Welcome to Dota 3 how to play!\n");
+        System.out.println("Step 1: Click 'Play Dota'. It will ask you to set difficulty");
+         System.out.println("Choose the enemies stats between");
+          System.out.println("Easy(damage: 0 - 10, HP:75)");
+           System.out.println("Medium(damage: 0 - 20, HP: 100");
+             System.out.println("Hard(damge: 0 - 30, HP: 150");
+              System.out.println("The player has a basic attack of 15-20 and an HP of 125");
+              System.out.println("The player has a skill attack which can only be used 3 times and has a damage of 0-20");
+              System.out.println("The player has 1 special attacks which has a damage of 50 and can only be used once");
+                 
+     System.out.println("\n\nStep 2: Choose your hero. There are 2 kinds of hero: Melee and Range");
+      System.out.println("Melee: 30% chance to bash the enemy");
+       System.out.println("Range: 20% chance add damage");
+           
+       
+       System.out.println("Important!!");
+       System.out.println("Bash: can make the opponent flinch and skips their turn");
+         System.out.println("add damage: adds a random plus damage of 10-20 of the hero's basic attack");
+    
+             System.out.println("Your opponent(AI) is a random hero");
+              
+    
+    
+    
+    
+ } 
+
+}
